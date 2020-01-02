8c49161d28fe301c789a50c73447ef19fe0f4486
s-odhiambo <samuelangienda1998@gmail.com>
2020-01-02 17:24:32 +0300

    add a sign up function to my views.py in my app

--- mysite/insta/views.py
+++ mysite/insta/views.py
@@ -13,5 +13,20 @@ from rest_framework import authentication, permissions
 
 
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
 
 # Create your views here.
